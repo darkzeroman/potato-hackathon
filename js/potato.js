f109b9e318592a6375839f5f4d1983d1ee8b2a4d
Vidhur Vohra <vidhur.vohra@wildfireapp.com>
2012-08-16 14:24:03 -0700

    final commit

--- js/potato.js
+++ js/potato.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
 
 	$('.submit').click(function(){
-		update();
+		reseteverything();
+		setTimeout(function(){
+			update();
+		}, 1000);
 	});
 	$('.controlstoggle').click(function(){
 
@@ -13,25 +16,80 @@ $(document).ready(function(){
 	});
 
 	$('.controlsrefresh').click(function(){
-		update();
+		reseteverything();
+		//update();
+
 	});
 
 	$('.test').click(function(){
-		$('.shades').animate({
-			height: 'toggle'
-		}, 800);
+		update();
 		//alert('lol');
 
 	});
+
+//	$('.test').click();
 
 	update();
 
 });
 
+function reseteverything(){
+
+	$('.potato-all').animate({
+		bottom: '1000px',
+	});
+	$('.blinds-retract').animate({
+		top: '0px'
+	}, 800);
+	$('.trophy-curtain').animate({
+		top: '133px'
+	}, 800);
+
+}
+
 function update(){
 	var obj = $('#controlsform').hashForm();
 
-	$('.marqueetext').text("I like " + obj['interest']);
+	$('.marqueetext').text("I love " + obj['interest']);
+
+	$('.potato-all').animate({
+		bottom: '123px',
+	}, 800).effect("bounce", { times: 3, distance: '50px'}, 600);
+	$('.blinds-retract').animate({
+		top: '-500px'
+	}, 800);
+	$('.trophy-curtain').animate({
+		top: '-500px'
+	}, 800);
+
+
+	var potatoImageURL;
+
+	if (obj['sex'] == "female")
+		potatoImageURL = "img/potato_adult-female.png";
+	else if (obj['age'] == "0-15")
+			potatoImageURL = "img/potato_boy.png";
+			//alert(potatoImageURL);
+	else 
+		potatoImageURL = "img/potato_adult.png";
+	
+
+	var numTrophies = obj['activitylevel'];
+
+
+	numPixels = 53;
+
+
+
+	$('.potato-all').css('background-image', 'url(' + potatoImageURL + ')');
+
+	$('.row1').css('width', numPixels*numTrophies).fadeIn();
+	$('.row2').css('width', numPixels*numTrophies).fadeIn();
+
+
+	$('.windowbackground-image').replaceWith("<img src=\"img/"+ obj['location'] +".png\" class=\"windowbackground-image\">");
+	
+
 
 
 };
